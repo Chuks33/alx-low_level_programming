7f54fb3ca887099d9efa77715d3466796ad0e073
Chuks33 <111119584+Chuks33@users.noreply.github.com>
2022-10-17 05:23:06 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,11 @@ int main(void)
 {
 
 char i;
-
-for (i = a; i <= z; i++)
-{
-if (i != q && i != e)
-putchar(i);
-}
-putchar(n);
-return (0);
+for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
