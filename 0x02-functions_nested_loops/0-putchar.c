c9b9ed73de2587db6d29acab6ec6c6f09dde60a4
Chuks33 <chuksfrank375@gmail.com>
2022-10-19 15:02:22 -0700

    putchar' commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -16,3 +16,22 @@ for (i = 0; string_[i] != '\0'; i++)
 _putchar('\n');
 	return (0);
 }
+#include "main.h"
+
+
+/**
+  * print_alphabet -> prints the lowercase alphabets
+  */
+
+void print_alphabet(void)
+{
+    int j;
+
+for (j = a ; j <= z ; j++)
+    {
+        _putchar(j);
+    }
+    _putchar (n);
+
+}
+
