e8de71fbefa3919200f5263ef722a1d05e70978b
Chuks33 <111119584+Chuks33@users.noreply.github.com>
2022-10-29 01:49:17 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,31 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
- * Return: void
-*/
+ *_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
+ */
 
 char *_strcat(char *dest, char *src)
 {
-         int i;
-         Int j;
+int i;
+int j;
 
-         i = 0;
-         while (dest[i] != '\0')
-         {
-                  i++;
-         }
-         j = 0;
-         while (src[j] != '\0')
-         {
-               dest[i] = src[j] != '\0')
-                  i++;
-                  j++;
-         {    
-         dest[i] = '\0';
-         return (dest);
+i = 0;
+while (dest[i] != '\0')
+{
+             i++;
+}
+
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+             j++;
+             i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
