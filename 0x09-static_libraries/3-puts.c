eec6e2b77c73c82fa391d4250b3280e5f59862c4
Chuks33 <111119584+Chuks33@users.noreply.github.com>
2022-11-09 00:56:25 +0100

    Create 3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line,
+ * Return: void
+ *@c: character input
+*/
+int _putchar(char c);
+
+void _puts(char *c)
+{
+	int i = 0;
+
+	while (c[i])
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+}
