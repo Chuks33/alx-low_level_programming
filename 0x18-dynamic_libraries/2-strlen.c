c23868d3311da1adb330254555a1b925a9662b94
Chuks33 <chuksfrank375@gmail.com>
2023-04-25 21:58:10 -0700

    0x1C-makefiles

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _strlen - function
+  *@s: input string
+  * Return: length of string
+  */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
